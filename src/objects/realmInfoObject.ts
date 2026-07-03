451872edd8d30f0181c7366e67553adf9dd05cc4
purifetchi <0xlunaric@gmail.com>
2026-07-03 14:46:07 +0200

    fix: do not set main camera when updating defaultCamera in realm_info.
    this is pure data

--- src/objects/realmInfoObject.ts
+++ src/objects/realmInfoObject.ts
@@ -1,4 +1,3 @@
-import { CameraObject } from ".";
 import { Serialized } from "../serialization";
 import { GameObject } from "./gameObject";
 
@@ -10,15 +9,5 @@ export class RealmInfoObject extends GameObject {
      * The default camera of this realm.
      */
     @Serialized("defaultCamera")
-    accessor defaultCamera: number = 0;
-
-    /**
-     * Called when a serialized property has changed.
-     */
-    onSerializedPropertyChanged(name: string): void {
-        const camera = this.game.getObjectById(this.defaultCamera)
-        if (camera !== undefined && camera instanceof CameraObject) {
-            this.game.setMainCamera(camera)
-        }
-    }
+    accessor defaultCamera: number | undefined = undefined
 }
